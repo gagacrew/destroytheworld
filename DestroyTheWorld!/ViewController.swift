6c30285b19762ce7fd7e5af10da780f856686304
Richard Albrecht <richard.albrecht@hamburg.de>
2017-05-11 11:49:24 +0200

    Initial Commit

---
+++ DestroyTheWorld!/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DestroyTheWorld!
+//
+//  Created by Richard Albrecht on 11.05.17.
+//  Copyright © 2017 abc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
